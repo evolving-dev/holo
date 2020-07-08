b37d3a702bba7395aea7c4aa0e0121d70f959866
Jannik Müller <jannik30013@gmail.com>
2020-07-08 16:15:33 +0200

    Improved readability of the static object initializer

--- scripts/init/initStaticCore.py
+++ scripts/init/initStaticCore.py
@@ -16,24 +16,41 @@ KEYMAP:dict = {"upper":{},"lower":{}}
 
 #LOADER OBJECT
 im = Image.open(join(PATH,"assets/animations/loading/loading.gif"))
+
 try:
+    
     while True:
+        
+        #Convert every frame into a separate image
         STATIC_CORE["loading"] += [pygame.image.fromstring(im.convert('RGBA').resize((SETTINGS["height"]//5,int(SETTINGS["height"]//5//2.746))).tobytes(),(SETTINGS["height"]//5,int(SETTINGS["height"]//5//2.746)),"RGBA").convert_alpha()]
         im.seek(im.tell()+1)
+        
 except EOFError:
-    del im
+    del im #Cleanup if all of the frames have been converted
+
 LOADERWIDTH = STATIC_CORE["loading"][0].get_width()
 TEXTCACHE = FONTS["p-sans-serif"].render(SYSTEM_TEXTS["loading"], True, [0,0,0] if SETTINGS["theme"] == "light" else [255,255,255])
+
 for i in range(len(STATIC_CORE["loading"])):
+    
     INITCACHE = STATIC_CORE["surface_4_1"].copy()
+    
+    #Blit Loading text and animation frame onto surface
     INITCACHE.blit(STATIC_CORE["loading"][i],(int(0.05*INITCACHE.get_width()),int(INITCACHE.get_height()//2)-STATIC_CORE["loading"][i].get_height() // 2))
     INITCACHE.blit(TEXTCACHE,(((int(INITCACHE.get_width()) - int(LOADERWIDTH*1.1)) // 2 - TEXTCACHE.get_width() // 2) + int(LOADERWIDTH*1.1) , int(INITCACHE.get_height()//2)-TEXTCACHE.get_height() // 2))
-    STATIC_CORE["loading"][i] = INITCACHE.copy()
+    
+    STATIC_CORE["loading"][i] = INITCACHE.copy()#Move cached frame into STATIC_CORE variable
+
+#CLEANUP LOADER
 del INITCACHE
 del TEXTCACHE
 del LOADERWIDTH
+
+
 ###
 #KEYBOARD
+
+#Load the Keytemplate and the special keys
 KEYTEMPLATE = pygame.image.fromstring(Image.open(join(PATH,"assets/images/icons/surface_1_1_"+SETTINGS["theme"]+".png")).resize((SETTINGS["width"]//15,SETTINGS["height"]//10)).tobytes(),(SETTINGS["width"]//15,SETTINGS["height"]//10),"RGBA").convert_alpha()
 SPACEBAR = pygame.image.fromstring(Image.open(join(PATH,"assets/images/icons/surface_8_1_"+SETTINGS["theme"]+".png")).resize(((SETTINGS["height"]//10)*8 if (SETTINGS["height"]//10)*8 < SETTINGS["width"] else SETTINGS["width"] // 1.5,SETTINGS["height"]//10)).tobytes(),((SETTINGS["height"]//10)*8 if (SETTINGS["height"]//10)*8 < SETTINGS["width"] else SETTINGS["width"] // 1.5,SETTINGS["height"]//10),"RGBA").convert_alpha()
 SHIFT = pygame.image.fromstring(Image.open(join(PATH,"assets/images/system/keyboard/shift-"+SETTINGS["theme"]+".png")).resize((SETTINGS["height"]//20,SETTINGS["height"]//20)).tobytes(),(SETTINGS["height"]//20,SETTINGS["height"]//20),"RGBA").convert_alpha()
@@ -41,9 +58,8 @@ BACKSPACE = pygame.image.fromstring(Image.open(join(PATH,"assets/images/system/k
 ENTER = pygame.image.fromstring(Image.open(join(PATH,"assets/images/system/keyboard/enter-"+SETTINGS["theme"]+".png")).resize((SETTINGS["height"]//20,SETTINGS["height"]//20)).tobytes(),(SETTINGS["height"]//20,SETTINGS["height"]//20),"RGBA").convert_alpha()
 BACKSPACE_SURFACE = pygame.image.fromstring(Image.open(join(PATH,"assets/images/icons/surface_2_1_"+SETTINGS["theme"]+".png")).resize((SETTINGS["height"]//5,SETTINGS["height"]//10)).tobytes(),(SETTINGS["height"]//5,SETTINGS["height"]//10),"RGBA").convert_alpha()
 
-#BLIT KEYS ONTO SURFACES
 
-
+#BLIT SPECIAL KEYS ONTO SURFACES
 KEYSURFACE = SHIFT.copy()
 SHIFT = KEYTEMPLATE.copy()
 SHIFT.blit(KEYSURFACE,(KEYTEMPLATE.get_width() // 2 - KEYSURFACE.get_width() // 2, KEYTEMPLATE.get_height() // 2 - KEYSURFACE.get_height() // 2))
@@ -54,70 +70,110 @@ KEYSURFACE = BACKSPACE.copy()
 BACKSPACE = BACKSPACE_SURFACE.copy()
 BACKSPACE.blit(KEYSURFACE,(BACKSPACE_SURFACE.get_width() // 2 - KEYSURFACE.get_width() // 2, BACKSPACE_SURFACE.get_height() // 2 - KEYSURFACE.get_height() // 2))
 
+#Cleanup to conserve memory
+del BACKSPACE_SURFACE
+del KEYSURFACE
 
-KEYBOARD_LAYOUT = eval(readfile(join(PATH,"assets/text/keyboard_layouts.json")))[SETTINGS["keyboard"]]
-BACKSPACE_DRAWN = False
+#################LOWERCASE KEYBOARD###################
+
+KEYBOARD_LAYOUT = eval(readfile(join(PATH,"assets/text/keyboard_layouts.json")))[SETTINGS["keyboard"]] #Load the lowercase keyboard layout
+
+BACKSPACE_DRAWN = False #Variable to compensate for the double width of the backspace character
 
 for ROW_INDEX,ROW_ITEM in enumerate(KEYBOARD_LAYOUT):
-    ROW = pygame.Surface([SETTINGS["width"],SETTINGS["height"] // 10], pygame.SRCALPHA).convert_alpha()
+    
+    ROW = pygame.Surface([SETTINGS["width"],SETTINGS["height"] // 10], pygame.SRCALPHA).convert_alpha() #Create new row
+    
     for i,char in enumerate(ROW_ITEM):
-        if char == "^":
+        
+        #SPECIAL CHARACTERS
+        if char == "^": #SHIFT
             ROW.blit(SHIFT, (i * SHIFT.get_width() + 2*i ,0))
             KEYMAP["lower"][char] = [[((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i),  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2], [((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i) + key.get_width(), key.get_height() +  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2]] #Add key to keymap
             continue
-        if char == "|":
+        
+        if char == "|": #ENTER
             ROW.blit(ENTER, (i * ENTER.get_width() + 2*i ,0))
             KEYMAP["lower"][char] = [[((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i),  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2], [((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i) + key.get_width(), key.get_height() +  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2]] #Add key to keymap
             continue
-        if char == "<":
+        
+        if char == "<":#Backspace
+            
             if not BACKSPACE_DRAWN:
+                
                 ROW.blit(BACKSPACE, (i * ENTER.get_width() + 2*i ,0)) #Width of normal key is used as a workaround
-                BACKSPACE_DRAWN = True
+                
+                BACKSPACE_DRAWN = True #Compensate for the doubled width of the backspace key
+                
                 KEYMAP["lower"]["<<"] = [[((SETTINGS["width"] // 2) - (len(ROW_ITEM) * ENTER.get_width()) // 2) + (i * ENTER.get_width() + 2*i),  (ROW_INDEX * BACKSPACE.get_height()) + SETTINGS["height"] // 2], [((SETTINGS["width"] // 2) - (len(ROW_ITEM) * ENTER.get_width()) // 2) + (i * ENTER.get_width() + 2*i) + BACKSPACE.get_width(), BACKSPACE.get_height() +  (ROW_INDEX * BACKSPACE.get_height()) + SETTINGS["height"] // 2]] #Add key to keymap
+            
             continue
-        key = KEYTEMPLATE.copy()
+        
+        key = KEYTEMPLATE.copy() #Load the surface on which the key will be blit
         
         KEYMAP["lower"][char] = [[((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i),  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2], [((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i) + key.get_width(), key.get_height() +  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2]] #Add key to keymap
         
-        letter = FONTS["p-sans-serif"].render(char,True,[0,0,0] if SETTINGS["theme"] == "light" else [255,255,255])
-        key.blit(letter,[key.get_width() // 2 - letter.get_width() // 2, key.get_height() // 2 - letter.get_height() // 2]) #Project text onto key
-        ROW.blit(key, (i * key.get_width() + 2*i ,0)) # Project key onto row
+        letter = FONTS["p-sans-serif"].render(char,True,[0,0,0] if SETTINGS["theme"] == "light" else [255,255,255]) #Render letter to blit onto the key
         
-    STATIC_CORE["keyboard_lower"].blit(ROW,((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2 ,ROW_INDEX * key.get_height())) #Project row onto keyboard
+        key.blit(letter,[key.get_width() // 2 - letter.get_width() // 2, key.get_height() // 2 - letter.get_height() // 2]) #Blit text onto key
+        ROW.blit(key, (i * key.get_width() + 2*i ,0)) # Blit key onto row
+        
+    STATIC_CORE["keyboard_lower"].blit(ROW,((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2 ,ROW_INDEX * key.get_height())) #Blit row onto keyboard
+
 STATIC_CORE["keyboard_lower"].blit(SPACEBAR, [SETTINGS["width"] // 2 - (SPACEBAR.get_width() // 2) ,4 * key.get_height()]) #Add the spacebar
 
 
-KEYBOARD_LAYOUT = eval(readfile(join(PATH,"assets/text/keyboard_layouts.json")))[SETTINGS["keyboard"]+"_upper"]
-BACKSPACE_DRAWN = False
+#################UPPERCASE KEYBOARD###################
+
+
+KEYBOARD_LAYOUT = eval(readfile(join(PATH,"assets/text/keyboard_layouts.json")))[SETTINGS["keyboard"]+"_upper"] #load the uppercase keyboard layout
+
+BACKSPACE_DRAWN = False #Variable to compensate for the double width of the backspace character
 
 for ROW_INDEX,ROW_ITEM in enumerate(KEYBOARD_LAYOUT):
-    ROW = pygame.Surface([SETTINGS["width"],SETTINGS["height"] // 10], pygame.SRCALPHA).convert_alpha()
+    
+    ROW = pygame.Surface([SETTINGS["width"],SETTINGS["height"] // 10], pygame.SRCALPHA).convert_alpha() #Create new row
+    
     for i,char in enumerate(ROW_ITEM):
+        
+        #SPECIAL CHARACTERS
+        
         if char == "^":
             KEYMAP["upper"][char] = [[((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i),  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2], [((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i) + key.get_width(), key.get_height() +  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2]] #Add key to keymap
             ROW.blit(SHIFT, (i * SHIFT.get_width() + 2*i ,0))
             continue
+        
         if char == "|":
             KEYMAP["upper"][char] = [[((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i),  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2], [((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i) + key.get_width(), key.get_height() +  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2]] #Add key to keymap
             ROW.blit(ENTER, (i * ENTER.get_width() + 2*i ,0))
             continue
+        
         if char == "<":
+            
             if not BACKSPACE_DRAWN:
+                
                 ROW.blit(BACKSPACE, (i * ENTER.get_width() + 2*i ,0)) #Width of normal key is used as a workaround
-                BACKSPACE_DRAWN = True
+                
+                BACKSPACE_DRAWN = True #Compensate for the doubled width of the backspace key
+                
                 KEYMAP["upper"]["<<"] = [[((SETTINGS["width"] // 2) - (len(ROW_ITEM) * ENTER.get_width()) // 2) + (i * ENTER.get_width() + 2*i),  (ROW_INDEX * BACKSPACE.get_height()) + SETTINGS["height"] // 2], [((SETTINGS["width"] // 2) - (len(ROW_ITEM) * ENTER.get_width()) // 2) + (i * ENTER.get_width() + 2*i) + BACKSPACE.get_width(), BACKSPACE.get_height() +  (ROW_INDEX * BACKSPACE.get_height()) + SETTINGS["height"] // 2]] #Add key to keymap
+            
             continue
-        key = KEYTEMPLATE.copy()
+        
+        key = KEYTEMPLATE.copy() #Load the surface on which the key will be blit
         
         KEYMAP["upper"][char] = [[((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i),  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2], [((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2) + (i * key.get_width() + 2*i) + key.get_width(), key.get_height() +  (ROW_INDEX * key.get_height()) + SETTINGS["height"] // 2]] #Add key to keymap
         
-        letter = FONTS["p-sans-serif"].render(char,True,[0,0,0] if SETTINGS["theme"] == "light" else [255,255,255])
-        key.blit(letter,[key.get_width() // 2 - letter.get_width() // 2, key.get_height() // 2 - letter.get_height() // 2]) #Project text onto key
-        ROW.blit(key, (i * key.get_width() + 2*i ,0)) # Project key onto row
-    STATIC_CORE["keyboard_upper"].blit(ROW,((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2 ,ROW_INDEX * key.get_height())) #Project row onto keyboard
+        letter = FONTS["p-sans-serif"].render(char,True,[0,0,0] if SETTINGS["theme"] == "light" else [255,255,255]) # Render letter to blit onto the key
+        
+        key.blit(letter,[key.get_width() // 2 - letter.get_width() // 2, key.get_height() // 2 - letter.get_height() // 2]) #Blit text onto key
+        ROW.blit(key, (i * key.get_width() + 2*i ,0)) # Blit key onto row
+        
+    STATIC_CORE["keyboard_upper"].blit(ROW,((SETTINGS["width"] // 2) - (len(ROW_ITEM) * key.get_width()) // 2 ,ROW_INDEX * key.get_height())) #Blit row onto keyboard
+    
 STATIC_CORE["keyboard_upper"].blit(SPACEBAR, [SETTINGS["width"] // 2 - (SPACEBAR.get_width() // 2) ,4 * key.get_height()]) #Add the spacebar
 
-del BACKSPACE_SURFACE
+#KEYBOARD CLEANUP
 del SPACEBAR
 del ROW_ITEM
 del ROW_INDEX
@@ -128,7 +184,8 @@ del key
 del letter
 del ROW
 del ENTER
-del KEYSURFACE
 del BACKSPACE
 del BACKSPACE_DRAWN
 del SHIFT
+
+################################END ONSCREEN KEYBOARD###################################
