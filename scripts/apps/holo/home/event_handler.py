ccf47d84b4221c38025d0bda73e82620ea5ef4eb
Jannik Müller <jannik30013@gmail.com>
2020-07-17 14:03:47 +0200

    Polished code documentation

--- scripts/apps/holo/home/event_handler.py
+++ scripts/apps/holo/home/event_handler.py
@@ -1,11 +1,11 @@
-if event.type == pygame.MOUSEBUTTONUP:
+if event.type == pygame.MOUSEBUTTONUP: #When the mousebutton is released
     mouse_pos = list(pygame.mouse.get_pos())
-    if len(ALERTS) == 0:
-        if not abs(data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0]) >= SETTINGS["width"] // 20:
-            for i in data["apps"]["areas"].keys():
+    if len(ALERTS) == 0: #When there are no alerts open
+        if not abs(data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0]) >= SETTINGS["width"] // 20: #When the mouse / finger has not been significantly moved on the x-axis
+            for i in data["apps"]["areas"].keys(): #Search for apps that might have been clicked
                 if data["page"] == data["apps"]["areas"][i][0]: #When the app found is on the currently active page
                     if mouse_pos[0] in range(data["apps"]["areas"][i][1][0],data["apps"]["areas"][i][2][0]) and mouse_pos[1] in range(data["apps"]["areas"][i][1][1], data["apps"]["areas"][i][2][1]):
-                        print(i)
+                        #When an app has been found, start it
                         holo.new_loader()
                         
                         #Update the loader manually
@@ -18,7 +18,7 @@ if event.type == pygame.MOUSEBUTTONUP:
                         APP = i
                         exec(APPLAUNCHER)
                         break
-        else:
+        else: #If a swipe has been detected
             if data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0] < 0:
                 data["page"] -= 1 if data["page"] > 0 else 0
             else:
--- scripts/apps/holo/home/update.py
+++ scripts/apps/holo/home/update.py
@@ -1,11 +1,11 @@
-screen.blit(data["assets"]["overlay"], (0,0))
+screen.blit(data["assets"]["overlay"], (0,0)) #Blit black / white overlay onto the background image
 
-screen.blit(data["surfaces"][data["page"]], (0,0))
+screen.blit(data["surfaces"][data["page"]], (0,0)) #Blit the current page onto the screen
 
 if not pygame.mouse.get_pressed()[0]:
-    data["cache"]["swipecache"] -= 1 if data["cache"]["swipecache"] > 0 else 0
+    data["cache"]["swipecache"] = 0 #Reset the swipecache if the mouse is no longer pressed
 
 if data["cache"]["swipecache"] == 0 and pygame.mouse.get_pressed()[0]:
-    data["cache"]["mouseposcache"] = list(pygame.mouse.get_pos())[0]
+    data["cache"]["mouseposcache"] = list(pygame.mouse.get_pos())[0] #Get x-coords of the mouse when the mouse button / finger is not held down
 
-data["cache"]["swipecache"] += pygame.mouse.get_pressed()[0]
+data["cache"]["swipecache"] += pygame.mouse.get_pressed()[0] #Save the time in frames in which the mousebutton has been held down
