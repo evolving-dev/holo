75834f9525c7856b51f2afad4e02dce61a64ed03
Jannik Müller <jannik30013@gmail.com>
2020-07-17 13:43:34 +0200

    Fast swipes are no longer detected as clicks

--- scripts/apps/holo/home/event_handler.py
+++ scripts/apps/holo/home/event_handler.py
@@ -1,7 +1,7 @@
 if event.type == pygame.MOUSEBUTTONUP:
     mouse_pos = list(pygame.mouse.get_pos())
     if len(ALERTS) == 0:
-        if not abs(data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0]) >= SETTINGS["width"] // 10:
+        if not abs(data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0]) >= SETTINGS["width"] // 20:
             for i in data["apps"]["areas"].keys():
                 if data["page"] == data["apps"]["areas"][i][0]: #When the app found is on the currently active page
                     if mouse_pos[0] in range(data["apps"]["areas"][i][1][0],data["apps"]["areas"][i][2][0]) and mouse_pos[1] in range(data["apps"]["areas"][i][1][1], data["apps"]["areas"][i][2][1]):
