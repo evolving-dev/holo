7fda860eec8e8f6590099e3e83ff71f6a5e13c56
Jannik Müller <jannik30013@gmail.com>
2020-07-17 13:41:54 +0200

    The menu pages will no longer change when closing alert boxes

--- scripts/apps/holo/home/event_handler.py
+++ scripts/apps/holo/home/event_handler.py
@@ -1,17 +1,25 @@
 if event.type == pygame.MOUSEBUTTONUP:
     mouse_pos = list(pygame.mouse.get_pos())
-    if not abs(data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0]) >= SETTINGS["width"] // 10:
-        for i in data["apps"]["areas"].keys():
-            if data["page"] == data["apps"]["areas"][i][0]: #When the app found is on the currently active page
-                if mouse_pos[0] in range(data["apps"]["areas"][i][1][0],data["apps"]["areas"][i][2][0]) and mouse_pos[1] in range(data["apps"]["areas"][i][1][1], data["apps"]["areas"][i][2][1]):
-                    print(i)
-                    holo.new_loader()
-                    pygame.display.flip()
-                    APP = i
-                    exec(APPLAUNCHER)
-                    break
-    else:
-        if data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0] < 0:
-            data["page"] -= 1 if data["page"] > 0 else 0
+    if len(ALERTS) == 0:
+        if not abs(data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0]) >= SETTINGS["width"] // 10:
+            for i in data["apps"]["areas"].keys():
+                if data["page"] == data["apps"]["areas"][i][0]: #When the app found is on the currently active page
+                    if mouse_pos[0] in range(data["apps"]["areas"][i][1][0],data["apps"]["areas"][i][2][0]) and mouse_pos[1] in range(data["apps"]["areas"][i][1][1], data["apps"]["areas"][i][2][1]):
+                        print(i)
+                        holo.new_loader()
+                        
+                        #Update the loader manually
+                        for loader in LOADERS:
+                            if not (FRAME % math.ceil(FPS / 6)):
+                                loader.update()
+                            screen.blit(loader.surface,tuple(loader.pos))
+                        
+                        pygame.display.flip()
+                        APP = i
+                        exec(APPLAUNCHER)
+                        break
         else:
-            data["page"] += 1 if data["page"] + 1 <= len(data["surfaces"]) - 1 else 0
+            if data["cache"]["mouseposcache"] - list(pygame.mouse.get_pos())[0] < 0:
+                data["page"] -= 1 if data["page"] > 0 else 0
+            else:
+                data["page"] += 1 if data["page"] + 1 <= len(data["surfaces"]) - 1 else 0
