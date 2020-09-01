178cab2e967d89295c8a7e9af26b5ac47dc39977
Jannik Müller <jannik30013@gmail.com>
2020-09-01 16:52:40 +0200

    Garbage collection

--- scripts/apps/holo/settings/__init__.py
+++ scripts/apps/holo/settings/__init__.py
@@ -8,6 +8,10 @@ data["assets"] = {
     "network": pygame.image.fromstring(Image.open(holo.path(join(APP_PATH["assets"] , "images/icons/network-" + SETTINGS["theme"] + ".png"))).resize((SETTINGS["height"] // 5,SETTINGS["height"] // 5)).tobytes(),(SETTINGS["height"] // 5,SETTINGS["height"] // 5),"RGBA").convert_alpha(),
     "troubleshooting": pygame.image.fromstring(Image.open(holo.path(join(APP_PATH["assets"] , "images/icons/troubleshooting-" + SETTINGS["theme"] + ".png"))).resize((SETTINGS["height"] // 5,SETTINGS["height"] // 5)).tobytes(),(SETTINGS["height"] // 5,SETTINGS["height"] // 5),"RGBA").convert_alpha(),
     "home":STATIC_CORE["home"],
+    "bottomBar": pygame.Surface([SETTINGS["width"], SETTINGS["height"] // 10]),
 }
+
+data["assets"]["bottomBar"].fill([0,0,0] if SETTINGS["theme"] == "dark" else [255,255,255])
+data["assets"]["bottomBar"].set_alpha(70)
 
 data["screen"] = "menu"
--- scripts/apps/holo/settings/update.py
+++ scripts/apps/holo/settings/update.py
@@ -1,7 +1,12 @@
 screen.blit(data["assets"]["overlay"], [0,0])
+
+screen.blit(data["assets"]["bottomBar"], [0, int(SETTINGS["height"] * 0.9)])
+
 screen.blit(data["assets"]["home"], [SETTINGS["width"] // 2 - data["assets"]["home"].get_width() // 2, SETTINGS["height"] // 20 - data["assets"]["home"].get_height() // 2 + int(SETTINGS["height"]*0.9)])
 
-
+if data["screen"] == "menu":
+    for i in range(5):
+        pygame.draw.line(screen, [128,128,128], [0, (i+1)*SETTINGS["height"] // 5], [SETTINGS["width"], (i+1)*SETTINGS["height"] // 5])
 
 
 
