e21cdbba17380d2d5a51cb248634a8aa9bc9c063
Jannik <jmapsyt@gmail.com>
2021-03-27 15:16:16 +0100

    Added conversion from absolute to relative path

--- scripts/core/holo_io.py
+++ scripts/core/holo_io.py
@@ -44,3 +44,23 @@ class holo_io:
             if PATH not in p:
                 return join(PATH,p) #Join system path and input path together
             return p
+
+        def to_relative(p): #Turn absolute path (e.g. /home/holo/holo/assets/example.xy or C:/Users/holo/Desktop/holo/assets/example.xy) into relative path (e.g. assets/example.xy)
+            if PATH in p:
+                p = p.replace(PATH, "") #Remove system path
+                return p[1:] if p[0] == "/" else p #remove slash at the first character
+            return p
+
+        def get_system_path():
+            return PATH
+
+        class app:
+
+            def get_asset_path(): #Will replace APP_PATH Variable in the future
+                pass
+
+            def get_script_path():
+                pass
+
+            def get_data_path():
+                pass
